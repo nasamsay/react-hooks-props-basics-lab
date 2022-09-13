359a55a5e0d2e8000215be9c885ee689c6d28c53
Nasam Sayour <nasamsay@gmail.com>
2022-09-13 19:26:21 +0300

    Complete assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
 import React from "react";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About(bio) {
+  if(!bio) {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+      </div>
+    )
+  }else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+      </div>
+    );
+  }
+  
 }
 
 export default About;
